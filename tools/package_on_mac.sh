ed4dd185a6e2682438f5961c3cfe3cba2bfed43e
wangzhi43 <wangzhi4388@gmail.com>
2026-03-20 08:29:33 +0800

    修改打包内容

--- tools/package_on_mac.sh
+++ tools/package_on_mac.sh
@@ -15,6 +15,10 @@ TMP_DIR="$(mktemp -d)"
 echo "Copying BitSoulStockSkill to temp dir: $TMP_DIR"
 cp -r "$SOURCE_DIR" "$TMP_DIR/BitSoulStockSkill"
 
+echo "Copying documentation files..."
+cp "$SCRIPT_DIR/../API_FOR_LLM.md" "$TMP_DIR/BitSoulStockSkill/"
+cp "$SCRIPT_DIR/../README.md" "$TMP_DIR/BitSoulStockSkill/"
+
 echo "Removing hidden files..."
 find "$TMP_DIR/BitSoulStockSkill" -name ".*" -delete
 
